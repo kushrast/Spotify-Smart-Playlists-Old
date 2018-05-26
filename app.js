6c7e8b3069b114a82962e64f4636b7d16b547dc7
Kush Rastogi <krustey@gmail.com>
2018-05-26 01:43:32 -0700

    forgot uri is a query param not a route param

--- app.js
+++ app.js
@@ -131,12 +131,12 @@ router.get('/callback', function(req, res, next) {
   }
 });
 
-router.get('/playlists/:uri', function(req, res, next) {
+router.get('/playlists/', function(req, res, next) {
 	console.log(req.session);
 	if (!req.session.access_token) {
 		res.redirect("/invalid");
 	} else {
-		var uri = req.params.uri.split(":");
+		var uri = req.query.uri.split(":");
 		var options = {
           url: 'https://api.spotify.com/v1/me/'+uri[2]+'/playlists/'+uri[4]+'/tracks',
           headers: { 'Authorization': 'Bearer ' + req.session.access_token },
